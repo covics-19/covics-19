cc20da3b84428a00bcd706e3c72cef0ca513c64b
alexn11 <alexn11.gh@gmail.com>
2020-04-26 11:02:40 +0100

    added comment on the update procedure

---
+++ src/data/hopkins_updater.py
@@ -0,0 +1,8 @@
+"""
+example of query for the update:
+https://api.covid19api.com/country/south-africa/status/confirmed?from=2020-03-01T00:00:00Z&to=2020-04-01T00:00:00Z
+
+Ive added a collection 'checkpoint' to get the date of the last update
+check for document with content such as:
+last_hopkins_update:"2020-04-25"
+"""
